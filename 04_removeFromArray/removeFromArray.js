d6706fd007246430fdc252e66408434d28849a1d
Karolis00 <alakvoscius.karolis@gmail.com>
2022-12-14 14:58:59 +0200

    04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray) {
+ //array.splice(index, 1)
+ //arguments.length = 3 (0 - array, 1 - /3, 2 - /2)
+ //originalArray.length = 4
 
+
+
+ for(let i = 0; i < originalArray.length; i++)
+ {
+    for(let j = 1; j < arguments.length; j++)
+    {
+        if(originalArray[i] === arguments[j]) 
+        {
+            originalArray.splice(i, 1);
+            i--;
+            break;
+        }
+    }
+ }
+ return originalArray;
 };
 
 // Do not edit below this line

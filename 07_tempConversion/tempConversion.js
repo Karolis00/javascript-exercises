63cb777e7a22e5f2b9c186d8e3646eb957f04ef0
Karolis00 <alakvoscius.karolis@gmail.com>
2022-12-14 15:35:38 +0200

    07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inFahrenheit) {
+  if(typeof inFahrenheit !== "number") return "ERROR";
+  return Math.round(((inFahrenheit - 32) * 5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inCelcius) {
+  if(typeof inCelcius !== "number") return "ERROR";
+  return Math.round((inCelcius * 9 / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line

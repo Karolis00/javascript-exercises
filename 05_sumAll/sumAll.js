2986aff29716d61f561ceb1dc2b43e9c03e9f7a6
Karolis00 <alakvoscius.karolis@gmail.com>
2022-12-14 15:07:21 +0200

    05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
+    if(typeof numberOne !== "number"
+    || typeof numberTwo !== "number"
+    || numberOne < 0 || numberTwo < 0)
+        return "ERROR";
 
+    let sum = 0;
+
+    if(numberTwo < numberOne)
+    {
+        let temp = numberOne;
+        numberOne = numberTwo;
+        numberTwo = temp;
+    }
+
+    for(let i = numberOne; i <= numberTwo; i++)
+    {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

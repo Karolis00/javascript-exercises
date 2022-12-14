b6cc49dc7a0700fc69baab36dda00c4a7e37ef20
Karolis00 <alakvoscius.karolis@gmail.com>
2022-12-14 14:41:35 +0200

    03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let newString = "";
+    if(typeof string !== "string") return "ERROR";
+     for(let i = string.length - 1; i >= 0; i--)
+     {
+        newString += string.charAt(i);
+     } 
+     return newString;
 };
 
 // Do not edit below this line

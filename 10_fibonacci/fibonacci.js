f817357904f6cffea31804425422084d20878562
Karolis00 <alakvoscius.karolis@gmail.com>
2023-07-03 13:56:26 +0300

    exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
 
+    if(x < 0) return "OOPS";
+    const fibo = [0, 1, 1];
+    for ( let i = 2; i < x; i++)
+    {
+        fibo.push(fibo[i] + fibo[i-1]);
+    }
+    return fibo[x];
 };
 
 // Do not edit below this line

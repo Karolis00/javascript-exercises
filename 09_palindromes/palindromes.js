eddbe977d04bb01b4905ea401c039cc6ac198c68
Karolis00 <alakvoscius.karolis@gmail.com>
2023-07-03 13:42:52 +0300

    exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    const letters = str.replace(/[^a-z0-9]/g, "").split('');
+    /*console.log(letters);
+    check = true;
+    for(let i = 0; i < letters.length/2; i++)
+    {
+        if(letters[i] !== letters[letters.length - 1 - i]) check = false;
+    }
+    return check;*/
+    return letters.join() === letters.reverse().join();
 };
 
 // Do not edit below this line

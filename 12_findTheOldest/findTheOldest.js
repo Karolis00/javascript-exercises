fe4125fab7faa22f9c7126f9ce38ed7223b90f9e
Karolis00 <alakvoscius.karolis@gmail.com>
2023-07-03 14:14:26 +0300

    Exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrOfPeople) {
+    const currentYear = (new Date()).getFullYear();
+    let oldest = arrOfPeople[0];
+    if(!('yearOfDeath' in oldest)) oldest.yearOfDeath = currentYear;
+    oldest.age = oldest.yearOfDeath - oldest.yearOfBirth;
+    const ages = arrOfPeople.map(person =>{
+        if(!('yearOfDeath' in person)) person.yearOfDeath = currentYear;
+        person.age = person.yearOfDeath - person.yearOfBirth;
+        if(person.age > oldest.age)
+        {
+            oldest = person;
+        }
+    })
+    return oldest;
 };
 
 // Do not edit below this line
